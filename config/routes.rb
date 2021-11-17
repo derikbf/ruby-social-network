3a4e33ba8534fddac2094b48c1ac7e60e7955141
derikbf <derikb@gmail.com>
2021-11-17 08:23:16 -0300

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
   
   namespace :admin do
-    root to: "home#index"
+    root to: 'home#index'
     # resources :admins
   end
 
