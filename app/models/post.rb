dc85cf770a64b475dad0ef8ea6aad48354dff392
derikbf <derikb@gmail.com>
2021-11-19 17:22:45 -0300

    Create Like

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
-
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  
+  def liked_by_user?(user)
+    likes.where(user: user).present?
+  end
+  
   validates :body, presence: true
 end
