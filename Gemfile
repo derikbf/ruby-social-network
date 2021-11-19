4d73b7f116bf509f111827f1170885282515b1df
derikbf <derikb@gmail.com>
2021-11-19 20:49:58 -0300

    Add Seeds

--- Gemfile
+++ Gemfile
@@ -33,3 +33,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'rspec-rails'
+gem 'faker'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :phone, :name, :avatar, presence: true
+  validates :phone, :name, presence: true
 
   has_one_attached :avatar
   has_many :posts
